e70b4ea2b2ee478b74d9fe2ac2f3bbedb95dc992
Varun Tyagi <varuntyagiben@gmail.com>
2026-02-10 22:16:01 +0530

    Auto update

--- Marks_Grade_Evaluation.java
+++ Marks_Grade_Evaluation.java
@@ -12,36 +12,37 @@ public class Marks_Grade_Evaluation {
         int RollNo = input.nextInt();
 
         System.out.print("Enter Your Grades: ");
-        float Grade = input.nextFloat();
-        if (Grade >= 0 && Grade <= 100);
-        System.out.print(" Invalid! Enter Correct marks again (0-100): ");
-
         while (true) {
-            if (Grade >= 90 && Grade <= 100) {
-                System.out.print("Your Grades: A+");
+            float Grade = input.nextFloat();
+            if (Grade >= 0 && Grade <= 100) break;
+            System.out.print(" Invalid! Enter Correct marks again (0-100): ");
+
+            while (true) {
+                if (Grade >= 90 && Grade <= 100) {
+                    System.out.print("Your Grades: A+");
 
 
-            } else if (Grade >= 80 && Grade <= 89) {
-                System.out.print("Your Grades: A ");
+                } else if (Grade >= 80 && Grade <= 89) {
+                    System.out.print("Your Grades: A ");
 
-            } else if (Grade >= 70 && Grade <= 79) {
-                System.out.print("Your Grades: B+ ");
+                } else if (Grade >= 70 && Grade <= 79) {
+                    System.out.print("Your Grades: B+ ");
 
-            } else if (Grade >= 60 && Grade <= 69) {
-                System.out.print("Your Grades: B ");
+                } else if (Grade >= 60 && Grade <= 69) {
+                    System.out.print("Your Grades: B ");
 
-            } else if (Grade >= 50 && Grade <= 59) {
-                System.out.print("Your Grades: C ");
+                } else if (Grade >= 50 && Grade <= 59) {
+                    System.out.print("Your Grades: C ");
 
-            } else if (Grade >= 45 && Grade <= 49) {
-                System.out.print("Your Grades: D ");
+                } else if (Grade >= 45 && Grade <= 49) {
+                    System.out.print("Your Grades: D ");
 
-            } else if (Grade >= 40 && Grade <= 44) {
-                System.out.print("Your Grades: E ");
+                } else if (Grade >= 40 && Grade <= 44) {
+                    System.out.print("Your Grades: E ");
 
+                }
+                input.close();
             }
-            input.close();
         }
     }
-
 }
