9166dce2da0048a5f4814d6cdf203a817741deee
Varun Tyagi <varuntyagiben@gmail.com>
2026-02-10 14:23:03 +0530

    Auto update

--- Marks_Grade_Evaluation.java
+++ Marks_Grade_Evaluation.java
@@ -1,7 +1,20 @@
 package Projects_Java;
+import java.util.Scanner;
 
 public class Marks_Grade_Evaluation {
     public static void main(String[] args) {
-        
+        Scanner input = new Scanner(System.in);
+
+        System.out.println("Enter Student Name");
+        String studentName = input.nextLine();
+
+        System.out.println("Enter Roll Number");
+        int RollNo = input.nextInt();
+
+        System.out.println("Enter Your Grades");
+        float Grade = input.nextFloat();
+
+
+
     }
 }
