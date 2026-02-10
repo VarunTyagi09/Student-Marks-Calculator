1a216ebad967944eea14705ce30510c15d7c36fe
Varun Tyagi <varuntyagiben@gmail.com>
2026-02-11 00:34:20 +0530

    Auto update

--- Calulator.java
+++ Calulator.java
@@ -1,5 +1,19 @@
 package Projects_Java;
 
+import java.util.Scanner;
+
 public class Calulator {
-    public static void main( String[] args)
+    public static void main( String[] args){
+        Scanner input = new Scanner(System.in);
+
+
+        System.out.print("Welcome to the Calculator: ");
+
+        System.out.print("What do you want to calculate: ");
+
+
+        
+
+
+    }
 }
